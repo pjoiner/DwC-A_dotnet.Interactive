da1302c6e103a54d6004c2641725db31bcf2e7bf
Paul Joiner <paul.joiner@aspiraconnect.com>
2021-11-23 09:50:03 -0600

    Added mapping example

---
+++ notebooks/Code/Multimedia.cs
@@ -0,0 +1,18 @@
+﻿public partial class Multimedia
+{
+    public string GbifID { get; set; }
+    public string Type { get; set; }
+    public string Format { get; set; }
+    public string Identifier { get; set; }
+    public string References { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Source { get; set; }
+    public string Audience { get; set; }
+    public string Created { get; set; }
+    public string Creator { get; set; }
+    public string Contributor { get; set; }
+    public string Publisher { get; set; }
+    public string License { get; set; }
+    public string RightsHolder { get; set; }
+}
