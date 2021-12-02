3ac44db2a0085e0209cef8dda9abf48a2d147fa2
Paul Joiner <paul.joiner@aspiraconnect.com>
2021-12-02 14:22:21 -0600

    Updated DataMapping notebook to use new dwca-codegen command.

--- notebooks/Code/Multimedia.cs
+++ notebooks/Code/Multimedia.cs
@@ -1,18 +1,10 @@
-﻿public partial class Multimedia
+﻿using System;
+
+public partial class Multimedia
 {
-    public string GbifID { get; set; }
+    public long GbifID { get; set; }
     public string Type { get; set; }
-    public string Format { get; set; }
     public string Identifier { get; set; }
-    public string References { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Source { get; set; }
-    public string Audience { get; set; }
-    public string Created { get; set; }
+    public DateTime Created { get; set; }
     public string Creator { get; set; }
-    public string Contributor { get; set; }
-    public string Publisher { get; set; }
-    public string License { get; set; }
-    public string RightsHolder { get; set; }
 }
